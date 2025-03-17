3852dd3cefaff3b9982f448b503804b61f83a783
theodomir <ukundasetheodomir@gmail.com>
2025-03-17 19:31:22 +0400

    third_version

--- program.py
+++ program.py
@@ -1,2 +1,12 @@
-x=input("Enter string:")
-print("the length of string is:", len(x))
+# x=input("Enter string:")
+# print("the length of string is:", len(x))
+
+# x=input("Enter string:")
+# print("the length of string is:", len(x.replace(" ","")))
+
+for i in range (1, 6):
+    x=input(print("Enter a any string:"))
+    j=len(x)
+    print(j)
+    i=i+1
+    
