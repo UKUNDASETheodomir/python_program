18af6e7a2ae53f0df997aae8cbb84a1d586779d5
theodomir <ukundasetheodomir@gmail.com>
2025-03-17 20:39:27 +0400

    fourth version

--- program.py
+++ program.py
@@ -4,9 +4,24 @@
 # x=input("Enter string:")
 # print("the length of string is:", len(x.replace(" ","")))
 
-for i in range (1, 6):
-    x=input(print("Enter a any string:"))
-    j=len(x)
-    print(j)
-    i=i+1
+# for i in range (1, 6):
+    # x=input(print("Enter a any string:"))
+    # j=len(x)
+    # print(j)
+    # i=i+1
+A = ['K', 'A', 'J', 'I', 'P', 'O', 'T', 'I', 'F', 'U']
+list=[]
+string=(input(print("Enter a any string:")))
+list.append(string)
+for i in list:
+     if (string.string[::]==A[::]):
+      print(A[o]=A[-1])
+ elif(string.string[::]!=A[::]):
+  print(A[0]=A[1]):
+     else:
+  print(A[0]=A[0])
+i=i+1m |
+
+
+
     
